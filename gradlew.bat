760f9d4d79993c48adc085e6d7a2d2ebbc187c74
kazimsayed954 <kazimsayed954@gmail.com>
2021-05-11 12:19:28 +0530

    android app added

---
+++ gradlew.bat
@@ -0,0 +1,44 @@
+{
+    "addPeople": {
+        "add": "Invita",
+        "countryNotSupported": "No est ancora possìbile de impreare custa destinatzione.",
+        "countryReminder": "Ses mutende in foras de is Istados Unidos? Verìfica chi insertas su còdighe de istadu!",
+        "disabled": "Non podes invitare gente.",
+        "failedToAdd": "Faddina in s'agiunta de partetzipantes",
+        "footerText": "Is mutidas in essida sunt disativadas.",
+        "loading": "Chirchende gente e nùmeros de telèfonu",
+        "loadingNumber": "Verifichende su nùmeru de telèfonu",
+        "loadingPeople": "Chirchende gente de invitare",
+        "noResults": "Non cointzidet perunu resultadu",
+        "noValidNumbers": "Inserta·nche unu nùmeru de telèfonu",
+        "searchNumbers": "Agiunghe nùmeros de telèfonu",
+        "searchPeople": "Chirca gente",
+        "searchPeopleAndNumbers": "Chirca gente o agiunghe is nùmeros de telèfonu issoro",
+        "telephone": "Telèfonu: {{number}}",
+        "title": "Invita gente a custa riunione"
+    },
+    "audioDevices": {
+        "bluetooth": "Bluetooth",
+        "headphones": "Auriculares",
+        "phone": "Telèfonu",
+        "speaker": "Altoparlante",
+        "none": "Perunu dispositivu de àudio a disponimentu"
+    },
+    "audioOnly": {
+        "audioOnly": "Àmpiu de banda bàsciu"
+    },
+    "calendarSync": {
+        "addMeetingURL": "Agiunghe unu ligàmene a s'addòbiu",
+        "confirmAddLink": "Boles agiùnghere unu ligàmene de Jitsi a custu addòbiu?",
+        "error": {
+            "appConfiguration": "S'integratzione de su calendàriu no est cunfigurada bene.",
+            "generic": "Faddina. Controlla sa cunfiguratzione de calendàriu o proa de atualizare su calendàriu.",
+            "notSignedIn": "Faddina in s'autenticatzione pro visualizare eventos de calendàriu. Controlla sa cunfiguratzione de calendàriu e proa de ti torrare a autenticare."
+        },
+        "join": "Aderi",
+        "joinTooltip": "Aderi a sa riunione",
+        "nextMeeting": "riunione imbeniente",
+        "noEvents": "Perunu eventu programmadu in futuru.",
+        "ongoingMeeting": "riunione in cursu",
+        "permissionButton": "Aberi sa cunfiguratzione",
+        "permissionMessage": "Su permissu de su ca
